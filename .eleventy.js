7f8037c7df69c415b42cfeb956e28c4c3c501d81
maureenlholland <maureen.holland11@gmail.com>
2020-03-15 21:30:12 -0400

    Basic styling, about copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats("html, liquid");
   eleventyConfig.addPassthroughCopy("img");
-  // eleventyConfig.addPassthroughCopy("styles.css.liquid");
 };
