4649da4aa51c159720783f4115b72ccadaeb54fc
maureenlholland <maureen.holland11@gmail.com>
2020-04-19 16:02:17 -0400

    add serviceworker to eleventy config, console log data

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats("html, liquid");
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("serviceworker.js");
 };
