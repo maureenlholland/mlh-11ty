1624fcad64afbede947f2d6eb4185eae7f9a86cf
maureenlholland <maureen.holland11@gmail.com>
2020-05-02 15:26:58 -0400

    add install and activate service worker events

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.setTemplateFormats("html, liquid");
   eleventyConfig.addPassthroughCopy("img");
-  // eleventyConfig.addPassthroughCopy("serviceworker.js");
+  eleventyConfig.addPassthroughCopy("serviceworker.js");
 
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if (outputPath.endsWith(".html")) {
